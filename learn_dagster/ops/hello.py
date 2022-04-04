21fe9374cdebdcb36ad8a36a36cd7379c3e164d9
Data Salaryman <datasalaryman@protonmail.com>
2022-04-04 17:14:17 +0800

    Add complicated TypeCheck example

--- learn_dagster/ops/hello.py
+++ learn_dagster/ops/hello.py
@@ -5,7 +5,8 @@ from dagster import (
     get_dagster_logger, 
     DagsterType, 
     In, 
-    Out
+    Out, 
+    TypeCheck
 )
 
 @op
@@ -60,10 +61,49 @@ def is_list_of_dicts(_, value):
         isinstance(element, dict) for element in value
     )
 
+def less_simple_data_frame_type_check(_, value):
+    if not isinstance(value, list):
+        return TypeCheck(
+            success=False,
+            description=f"LessSimpleDataFrame should be a list of dicts, got {type(value)}",
+        )
+
+    fields = [field for field in value[0].keys()]
+
+    for i in range(len(value)):
+        row = value[i]
+        idx = i + 1
+        if not isinstance(row, dict):
+            return TypeCheck(
+                success=False,
+                description=(
+                    f"LessSimpleDataFrame should be a list of dicts, got {type(row)} for row {idx}"
+                ),
+            )
+        row_fields = [field for field in row.keys()]
+        if fields != row_fields:
+            return TypeCheck(
+                success=False,
+                description=(
+                    f"Rows in LessSimpleDataFrame should have the same fields, got {row_fields} "
+                    f"for row {idx}, expected {fields}"
+                ),
+            )
+
+    return TypeCheck(
+        success=True,
+        description="LessSimpleDataFrame summary statistics",
+        metadata={
+            "n_rows": len(value),
+            "n_cols": len(value[0].keys()) if len(value) > 0 else 0,
+            "column_names": str(list(value[0].keys()) if len(value) > 0 else []),
+        },
+    )
+
 
 SimpleDataFrame = DagsterType(
     name="SimpleDataFrame",
-    type_check_fn=is_list_of_dicts,
+    type_check_fn=less_simple_data_frame_type_check,
     description="A naive representation of a data frame, e.g., as returned by csv.DictReader.",
 )
 
